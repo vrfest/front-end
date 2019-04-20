653f7f496341fb65a4fdbf446454adafba83a824
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-20 03:10:17 -0700

    Add logo image

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../constants/login.css';
+import image from '../constants/images/logo.png';
 
 export default class LandingPage extends React.Component {
     render() {
         return (
             <div className="login">
                 <div className="login-box">
-                    <img src="https://cdn.shopifycloud.com/hatchful-web/assets/2adcef6c1f7ab8a256ebdeba7fceb19f.png" alt="Logo" />
+                    <img src={image} alt="Logo" />
 
                     <h1 className="h1">Your Community Awaits</h1>
 
@@ -20,11 +21,11 @@ export default class LandingPage extends React.Component {
                             <label for="re-me">Remember Me</label>
                         </div>
 
-                        <a href="https://google.com"> Forgot Your Password?</a>
+                        <a href="#"> Forgot Your Password?</a>
                     </div>
                     <button>Sign In</button>
 
-                    <p>Don't have an VRFest account? <a href="https://google.com">Sign Up</a></p>
+                    <p>Don't have an VRFest account? <a href="/signup">Sign Up</a></p>
                 </div>
             </div>
         )
