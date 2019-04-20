286fca2ab4abf8f9a28aa00077481f1ccd831b15
VietDinh17 <vietqudinh@gmail.com>
2019-04-19 22:43:10 -0700

    Add package

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-// import './constants/css/custome-styles.css';
+import './constants/custome-styles.css';
 
 ReactDOM.render((
     <BrowserRouter>
