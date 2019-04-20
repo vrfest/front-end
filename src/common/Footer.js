7abe9875c4f969f5b104c0b41b297518d578da6e
VietDinh17 <vietqudinh@gmail.com>
2019-04-20 01:15:03 -0700

    Add landing page, footer, and header

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { Paper, Grid, InputBase } from '@material-ui/core';
 
 export default class Footer extends React.Component{
     render(){
         return(
-            <div>
-                Footer
-            </div>
+            <Paper className="footer-container" square={true}>
+                <Grid direction="column" container>
+                    <Grid container direction="row" justify="space-evenly" style={{marginTop:'4%'}}>
+                        <div>
+                        <Grid container direction="column">
+                            <h4 style={{color:'white'}}>Quick Links</h4>
+                            <h6 className="footer-link">Data</h6>
+                            <h6 className="footer-link">Recomendation</h6>
+                            <h6 className="footer-link">Map</h6>
+                        </Grid>
+                        </div>
+                        <div>
+                        <Grid container direction="column">
+                            <h4 style={{color:'white'}}>Features</h4>
+                            <h6 className="footer-link">Data</h6>
+                            <h6 className="footer-link">Recomendation</h6>
+                            <h6 className="footer-link">Map</h6>
+                        </Grid>
+                        </div>
+                        <div>
+                        <Grid container direction="column">
+                            <h4 style={{color:'white'}}>Resources</h4>
+                            <h6 className="footer-link">Data</h6>
+                            <h6 className="footer-link">Recomendation</h6>
+                            <h6 className="footer-link">Map</h6>
+                        </Grid>
+                        </div>
+                        <div>
+                        <Grid container direction="column">
+                            <h4 style={{color:'white'}}>Newsletter</h4>
+                            <h6 className="footer-link">You can trust us. we only send promo offers</h6>
+                            <Paper className="footer-newleter" square={true}>
+                                <Grid container direction="row" justify="space-between">
+                                <InputBase className="footer-input" placeholder="Your Email Address" />
+                                <Paper square={true} className="footer-button">
+                                </Paper> 
+                                </Grid>
+                            </Paper>
+                        </Grid>
+                        </div>
+                    </Grid>
+                    <Grid container direction="row" style={{marginTop:10}}>
+                        <p className="copy-footer">
+                            Copyright &copy;{new Date().getFullYear()} 
+                            All rights reserved
+                        </p>
+                        <div>
+                        <Grid container direction="row" justify="space-evenly">
+                            {/* <SocialIcon url="http://facebook.com/jaketrent"/>    
+                            <SocialIcon url="http://twitter.com/jaketrent"/>
+                            <SocialIcon url="http://instagram.com/jaketrent"/> */}
+                        </Grid>
+                        </div>      
+                    </Grid>
+                </Grid>
+            </Paper>
         )
     }
 }
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Grid, Paper, List } from '@material-ui/core';
+import SmallCard from '../components/SmallCard';
 
 export default class LandingPage extends React.Component{
     render(){
         return(
-            <div>
-                LandingPage
-            </div>
+            <Paper className="landing-paper" square={true}>
+                <Paper className="landing-paper-top" square={true}>
+                </Paper>
+                <Paper className="landing-paper-bot" square={true}>
+                    <Grid>
+
+                    </Grid>
+                    <div style={{marginLeft: '10%', marginTop: '2%'}}>
+                    <List className="small-card-list">
+                        <div className="small-card-region">
+                            <SmallCard />
+                        </div>
+
+                        <div className="small-card-region">
+                            <SmallCard />
+                        </div>
+                        <div className="small-card-region">
+                            <SmallCard />
+                        </div>
+                        <div className="small-card-region">
+                            <SmallCard />
+                        </div>
+                    </List>
+                    </div>  
+                </Paper>
+            </Paper>        
         )
     }
 }
