ccde3ee005d5f8b5078a88c35b527b10c284f9ba
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-20 15:47:37 -0700

    Tweak pages (Dash and Landing)

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -3,27 +3,34 @@ import { Grid } from '@material-ui/core';
 import SmallCard from '../components/SmallCard';
 import { artists } from '../constants/initialState.json';
 
-export default class Dashboard extends React.Component{
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+
+export default class Dashboard extends React.Component {
     renderArtist = (artist) => (
         <div key={artist.name} className="small-card-region">
-            <SmallCard artist={artist}/>
+            <SmallCard artist={artist} />
         </div>
     )
-    render(){
-        return(
-            <div className="dashboard-container">
-                <h2 className="header-date">Upcoming Events</h2>
-                <Grid container direction="column">
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="dashboard-container">
+                    <h2 className="header-date">Upcoming Events</h2>
+                    <Grid container direction="column">
                         {
                             artists.map(artist => (this.renderArtist(artist)))
                         }
-                </Grid>
-                <h2 className="header-date">New Artists</h2>
-                <Grid container direction="column">
+                    </Grid>
+                    <h2 className="header-date">New Artists</h2>
+                    <Grid container direction="column">
                         {
                             artists.map(artist => (this.renderArtist(artist)))
                         }
-                </Grid>
+                    </Grid>
+                </div>
+                <Footer />
             </div>
         )
     }
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Grid, Paper, InputBase } from '@material-ui/core';
 import '../constants/landing.css';
 import { FirebaseDatabaseMutation } from '@react-firebase/database';
 import image from '../constants/images/vrlogo.png';
@@ -8,15 +7,15 @@ import wordLogo from '../constants/images/vrfest.png';
 const path = "user_emails";
 
 export default class LandingPage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            email : null,
+            email: null,
         }
     }
 
     handleChange = (event) => {
-        this.setState({email: event.target.value});
+        this.setState({ email: event.target.value });
     }
 
     render() {
@@ -35,36 +34,22 @@ export default class LandingPage extends React.Component {
                         <FirebaseDatabaseMutation type="push" path={path}>
                             {({ runMutation }) => {
                                 return (
-                                <div>
-                                    <button className="landing-button"
-                                    data-testid="test-push"
-                                    onClick={async () => {
-                                        const { key } = await runMutation({ email : this.state.email });
-                                    }}
-                                    >
-                                    Sign up
+                                    <div>
+                                        <button className="landing-button"
+                                            data-testid="test-push"
+                                            onClick={async () => {
+                                                const { key } = await runMutation({ email: this.state.email });
+                                            }}
+                                        >
+                                            Sign up
                                     </button>
-                                </div>
+                                    </div>
                                 );
                             }}
                         </FirebaseDatabaseMutation>
                     </form>
 
                 </div>
-                {/* <Grid container direction="column" alignItems="center">
-                    <h3 className="banner-title"> VRFest </h3>
-                    <h6 style={{ marginTop: '2%' }}>VR festivals and concerts with your favourite artists.</h6>
-                    <h6> Sign up to the waitlist for $10 off your first ticket.</h6>
-                    <Paper className="home-paper-search">
-                        <InputBase className="home-input" placeholder="Enter your email here" />
-                        <Paper className="home-input-paper" square={true} onClick={() => console.log("click")}>
-                            <h6 className="home-header" style={{ marginTop: 10 }}>Submit</h6>
-                        </Paper>
-                    </Paper>
-                </Grid>
-                <div className="landing-bottom">
-
-                </div> */}
             </div>
         )
     }
