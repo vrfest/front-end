393b1f1562cf988ba35ddc8a8641eaec3189e180
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-20 05:01:47 -0700

    Update UI navbar

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { Paper, Grid, Typography } from '@material-ui/core';
+import '../constants/header.css';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <Paper className="header-navbar" square={true}>
-                <Grid container alignItems="center" justify="center" direction="column">
-                    {/* <div> */}
-                    <Grid className="header-box" container direction="row" justify="space-between">
-                        <Grid>☰</Grid>
-                        <Grid>Brand</Grid>
-                        <div className="header-login-box">
-                            <Grid className="header-login-box-child" container direction="row" justify="space-around">
-                                <Typography style={{ marginRight: 10 }}>Login</Typography>
-                                <Typography>Logout</Typography>
-                            </Grid>
-                        </div>
-                    </Grid>
-                    {/* </div> */}
+            <div className="navbar">
+                <div className="navbar-main">
+                    <div className="navbar-left">
+                        <p>☰</p>
+                    </div>
 
-                </Grid>
-
-            </Paper>
+                    <div className="navbar-right">
+                        <p>Login / Signup</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
