d520fa4ac0f187f14e59c916b96d1803157cb479
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-20 19:07:33 -0700

    Update dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -3,33 +3,58 @@ import { Grid } from '@material-ui/core';
 import SmallCard from '../components/SmallCard';
 import { artists } from '../constants/initialState.json';
 
+import '../constants/dashboard.css';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 
 export default class Dashboard extends React.Component {
-    renderArtist = (artist) => (
-        <div key={artist.name} className="small-card-region">
-            <SmallCard artist={artist} />
-        </div>
-    )
+    renderArtist = () => {
+        return artists.map((artist) => {
+            return (
+                <div className="home-card">
+                    <img className="home-card-img" src={artist.image} />
+
+                    <div className="home-card-text">
+                        <h3 className="home-h3">{artist.name}</h3>
+                        <h4 className="home-h4">{artist.date}</h4>
+                        <p className="home-p">{artist.content}</p>
+                    </div>
+
+                    <div className="home-card-button">
+                        <h3 className="home-h3">{artist.cost + '$'}</h3>
+                        <button className="home-button">Buy Now</button>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
         return (
             <div>
                 <Header />
-                <div className="dashboard-container">
-                    <h2 className="header-date">Upcoming Events</h2>
-                    <Grid container direction="column">
-                        {
-                            artists.map(artist => (this.renderArtist(artist)))
-                        }
-                    </Grid>
-                    <h2 className="header-date">New Artists</h2>
-                    <Grid container direction="column">
-                        {
-                            artists.map(artist => (this.renderArtist(artist)))
-                        }
-                    </Grid>
+                <div className="dashboard">
+                    <h2 className="dashboard-h2">My tickets</h2>
+                    <div className="dashboard-feature">
+                        <img className="dashboard-img" src="http://www.stickpng.com/assets/images/5bb36021554c7f08176ec0e4.png" />
+                        <img className="dashboard-img" src="http://www.stickpng.com/assets/images/5bb36021554c7f08176ec0e4.png" />
+                        <img className="dashboard-img" src="http://www.stickpng.com/assets/images/5bb36021554c7f08176ec0e4.png" />
+                    </div>
+
+                    <div className="dashboard-feature-time">
+                        <p>Coming live in: <b>25 min 40 sec</b></p>
+                        <button>Live Now</button>
+                        <p>Coming live in: <b>25 min 40 sec</b></p>
+                    </div>
+
+                    <h2 className="dashboard-h2">Upcoming events</h2>
+                    {this.renderArtist()}
+
+                    <h2 className="dashboard-h2">New Artists</h2>
+                    {this.renderArtist()}
+
                 </div>
+
                 <Footer />
             </div>
         )
