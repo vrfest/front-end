9b469600684c29667a62eb4b757a9a5768f97afd
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-20 18:09:44 -0700

    Update header and footer

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,31 +2,38 @@ import React from 'react';
 import '../constants/login.css';
 import image from '../constants/images/logo.png';
 
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+
 export default class LandingPage extends React.Component {
     render() {
         return (
-            <div className="login">
-                <div className="login-box">
-                    <img className="form-logo" src={image} alt="Logo" />
+            <div>
+                <Header />
+                <div className="login">
+                    <div className="login-box">
+                        <img className="form-logo" src={image} alt="Logo" />
 
-                    <h1 className="h1">Your Community Awaits</h1>
+                        <h1 className="h1">Your Community Awaits</h1>
 
-                    <p className="label">Sign In</p>
-                    <input type="email" name="email" placeholder="Email" required />
-                    <input type="password" name="password" placeholder="Password" required />
+                        <p className="label">Sign In</p>
+                        <input type="email" name="email" placeholder="Email" required />
+                        <input type="password" name="password" placeholder="Password" required />
 
-                    <div className="form-row">
-                        <div className="form-row2">
-                            <input type="checkbox" id="re-me" placeholder="Password" />
-                            <label for="re-me">Remember Me</label>
+                        <div className="form-row">
+                            <div className="form-row2">
+                                <input type="checkbox" id="re-me" placeholder="Password" />
+                                <label for="re-me">Remember Me</label>
+                            </div>
+
+                            <a href="/signup"> Forgot Your Password?</a>
                         </div>
+                        <button className="btn-signin">Sign In</button>
 
-                        <a href="/signup"> Forgot Your Password?</a>
+                        <p>Don't have an VRFest account? <a href="/signup">Sign Up</a></p>
                     </div>
-                    <button className="btn-signin">Sign In</button>
-
-                    <p>Don't have an VRFest account? <a href="/signup">Sign Up</a></p>
                 </div>
+                <Footer />
             </div>
         )
     }
--- src/views/Signup.js
+++ src/views/Signup.js
@@ -2,26 +2,33 @@ import React from 'react';
 import '../constants/signup.css';
 import image from '../constants/images/logo.png';
 
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+
 export default class LandingPage extends React.Component {
     render() {
         return (
-            <div className="signup">
-                <div className="signup-box">
-                    <img src={image} alt="Logo" />
+            <div>
+                <Header />
+                <div className="signup">
+                    <div className="signup-box">
+                        <img src={image} alt="Logo" />
 
-                    <h1 className="h1">Create Account</h1>
+                        <h1 className="h1">Create Account</h1>
 
-                    <p className="label">Sign Up</p>
-                    <input type="text" name="fname" placeholder="*First Name" required />
-                    <input type="text" name="lname" placeholder="*Last Name" required />
-                    <input type="text" name="username" placeholder="*Username" required />
-                    <input type="email" name="email" placeholder="*Email" required />
-                    <input type="password" name="password" placeholder="*Password" required />
+                        <p className="label">Sign Up</p>
+                        <input type="text" name="fname" placeholder="*First Name" required />
+                        <input type="text" name="lname" placeholder="*Last Name" required />
+                        <input type="text" name="username" placeholder="*Username" required />
+                        <input type="email" name="email" placeholder="*Email" required />
+                        <input type="password" name="password" placeholder="*Password" required />
 
-                    <button className="btn-signin">Sign Up</button>
+                        <button className="btn-signin">Sign Up</button>
 
-                    <p>Have an VRFest account? <a href="/login">Sign In</a></p>
+                        <p>Have an VRFest account? <a href="/login">Sign In</a></p>
+                    </div>
                 </div>
+                <Footer />
             </div>
         )
     }
